fa5a7820b1b933bee6573861e44ad8cfdab32dad
Raphaël <raphael.Merkling@gmail.com>
2015-05-29 15:59:57 +0200

    Ajout de fonction dans l'algorithme Génétique

--- src/algorithmes/genetique/Genetique.java
+++ src/algorithmes/genetique/Genetique.java
@@ -1,6 +1,7 @@
 package algorithmes.genetique;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import model.DonneeAlgo;
 import model.Model;
@@ -9,9 +10,13 @@ import algorithmes.IAlgo;
 
 public class Genetique implements IAlgo {
 
-	protected ArrayList<Clavier> listeClaviers;
 	protected Model model;
-	
+	protected ArrayList<Clavier> listeClaviers;
+	protected int population;
+	protected int percentMutation;
+	protected int percentCroissement;
+	protected int iteration;
+
 	public Genetique(Model model) {
 		this.model = model;
 	}
@@ -20,38 +25,81 @@ public class Genetique implements IAlgo {
 	public DonneeAlgo getMeilleurClavier() {
 		initialisation();
 		do {
-			evaluation();
+			iteration++;
+			evaluation(); // ici, elle est faite dans la séléction
 			selection();
-			croisement();
+			croisement(); // On n'en fait pas pour le moment
 			mutation();
 		} while (arret());
-		return null;
+		Clavier best = getMeilleur();
+		return new DonneeAlgo(best, iteration);
 	}
 
 	public void initialisation() {
+		iteration = 0;
+		population = model.getPopulation();
+		percentMutation = model.getMutation();
+		percentCroissement = 0;
 		listeClaviers = new ArrayList<Clavier>();
 		for (int i = 0; i < model.getPopulation(); i++) {
 			listeClaviers.add(new Clavier());
 		}
 	}
-	
+
 	public void evaluation() {
-		
+
 	}
-	
+
 	public void selection() {
-		
+		ArrayList<Clavier> selection = new ArrayList<Clavier>();
+		Random r = new Random();
+		Clavier best, tmp;
+		int selec, eval, newEval;
+		for (int i = 0; i < population; i++) {
+			best = null;
+			tmp = null;
+			eval = Integer.MAX_VALUE;
+			for (int j = 0; j < population / 2; j++) {
+				selec = r.nextInt(population);
+				tmp = listeClaviers.get(selec);
+				newEval = tmp.evaluation();
+				if (newEval < eval) {
+					eval = newEval;
+					best = tmp;
+				}
+			}
+			selection.add(best);
+		}
+		listeClaviers = selection;
 	}
-	
+
 	public void croisement() {
-		
+
 	}
-	
+
+	public Clavier getMeilleur() {
+		Clavier best = null;
+		int newEval;
+		int eval = Integer.MAX_VALUE;
+		for (Clavier c : listeClaviers) {
+			newEval = c.evaluation();
+			if (newEval < eval) {
+				eval = newEval;
+				best = c;
+			}
+		}
+		return best;
+	}
+
 	public void mutation() {
-		
+		Clavier c;
+		for (int i = 0; i < (percentMutation * population / 100); i++) {
+			c = listeClaviers.get(i);
+			listeClaviers.set(i, c.mutation());
+		}
 	}
-	
+
 	public boolean arret() {
-		return true;
+		return (iteration < 100);
 	}
 }
